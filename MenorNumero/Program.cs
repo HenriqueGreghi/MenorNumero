72836fc5c6e9436c5f9560c74d8b854a14df8128
Henrique Greghi <henriquevsg@gmail.com>
2022-08-21 21:37:25 -0300

    Verificando menor numero

---
+++ MenorNumero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MenorNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menor = 0;
+            int posicao = 0;
+            Console.WriteLine("Digite a quantidade de elemetos:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[n];
+            string[] numeros = Console.ReadLine().Split(' ');
+            for (int i=0; i< n; i++)
+            {
+                vet[i] = int.Parse(numeros[i]);
+            }
+            for (int i = 0; i <n; i++)
+            {
+                if (vet[i] < menor)
+                {
+                    menor = vet[i];
+                    posicao = i;
+                }
+                else if (i == 0)
+                {
+                    menor = vet[i];
+                    posicao = i;
+                }
+            }
+            Console.WriteLine("Menor número: " + menor);
+            Console.WriteLine("Posição: "+ posicao);
+        }
+    }
+}
